7b5fdbdf5cb6a05642bfa2fc128b5079313faa73
hjzheld <dusen7774@naver.com>
2026-01-11 11:25:21 +0900

    라우터 재수정

--- src/App.tsx
+++ src/App.tsx
@@ -46,7 +46,7 @@ function AppContent() {
       <Header />
       <main>
         <Routes>
-          <Route path="/MainPage/" element={<Navigate to="/home" replace />} />
+          <Route path="/MainPage/" element={<Navigate to="/MainPage/home" replace />} />
           <Route path="/MainPage/home" element={<MainPage />} />
           <Route path="/MainPage/project" element={<PortfolioPage />} />
           <Route path="/MainPage/project/:id" element={<Detail />} />
